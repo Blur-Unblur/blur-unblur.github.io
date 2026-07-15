249a8f92012625c2f3c8ab5b2f24a5206dbc2df0
jstoolsspace <jstoolsspace@proton.me>
2026-07-15 03:19:20 +0300

    Deploy site to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  site: "https://blur-unblur.github.io",
   vite: {
     plugins: [tailwindcss()],
   },
